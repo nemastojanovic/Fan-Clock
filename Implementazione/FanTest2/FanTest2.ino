20213d2a04efb17b691b353b8072dafcbb50601a
dianaLiloia <31542203+dianaLiloia@users.noreply.github.com>
2017-12-15 11:29:47 +0100

    Test velocità ventola

---
+++ Implementazione/FanTest2/FanTest2.ino
@@ -0,0 +1,21 @@
+long s = 0;
+long interval = 0;
+int verde = 2 ;
+
+void setup() {
+  pinMode(verde, INPUT_PULLUP);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(verde), t, RISING);
+}
+
+void loop(){
+}
+
+void t(){
+  interval = millis()-s;
+  Serial.println(interval);
+  s = millis();
+  interval = 0;
+}
+
+
