3cdf5c22ad800175ef0330762762283bda6457cc
dianaLiloia <31542203+dianaLiloia@users.noreply.github.com>
2017-12-15 10:49:26 +0100

    Impulso ventola test

---
+++ Implementazione/FanTest/FanTest.ino
@@ -0,0 +1,22 @@
+
+int verde = 2 ;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(verde, INPUT);
+  Serial.begin(9600);
+  //La costante RISING fa in modo che l'interrup legga il valore 
+  // quando il pin passa dal basso verso l'alto.
+  attachInterrupt(digitalPinToInterrupt(verde), m, RISING);
+}
+
+void loop(){
+  delay(500);
+}
+
+void m(){
+  //scrivo il vaore sul monitor seriale
+  Serial.println(digitalRead(verde));
+}
+
+
