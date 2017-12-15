49d3faa585a552b45911004815e4d7bdab0ba114
dianaLiloia <31542203+dianaLiloia@users.noreply.github.com>
2017-12-15 13:27:41 +0100

    Update FanTest2.ino

--- Implementazione/FanTest2/FanTest2.ino
+++ Implementazione/FanTest2/FanTest2.ino
@@ -5,6 +5,8 @@ int verde = 2 ;
 void setup() {
   pinMode(verde, INPUT_PULLUP);
   Serial.begin(9600);
+  //La costante RISING fa in modo che l'interrupt legga il valore 
+  // quando il pin passa dal basso verso l'alto.
   attachInterrupt(digitalPinToInterrupt(verde), t, RISING);
 }
 
@@ -12,6 +14,7 @@ void loop(){
 }
 
 void t(){
+  //Scrivo sul monitor seriale la velocità con cui fa un giro la ventola
   interval = millis()-s;
   Serial.println(interval);
   s = millis();
